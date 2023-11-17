d8c19ff9c7ea6ed981f60c2e2289fd886f3d7c71
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-17 19:43:43 +0200

    first commit

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Method(int a)
+{
+    Console.WriteLine("Method 1");
+}
+
+// static void Method(string a)
+// {
+//     Console.WriteLine("Method 2");
+// }
+
+Method(42);       // Calls the version with int parameter
